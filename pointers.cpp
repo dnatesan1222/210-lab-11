91c52e53687af366768ad1ee6a4a475c87bb7315
Diksha <diksha@macbookpro>
2025-09-21 13:06:53 -0700

    update

--- pointers.cpp
+++ pointers.cpp
@@ -1,2 +1,19 @@
 // COMSC-210 | Lab 12 | Justin Beiber (your name)
 // IDE used: Vim (Terminal) 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+struct Customer{ //bakery's database of customer rewards/info
+    //customer name
+
+    //these next three variables update every time an order comes through
+    //double for total money spent at the bakery
+    //rewards accumulated
+    //dynamic array of a customer's order history
+
+}
+
+

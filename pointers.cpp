c6ac76274041c87cf6d0407f3e69c82c671d7860
Diksha <diksha@macbookpro>
2025-09-21 18:53:51 -0700

    starting edits again

--- pointers.cpp
+++ pointers.cpp
@@ -6,70 +6,81 @@
 using namespace std;
 
 
-const int SIZE = 100;
 
 
 struct Order{ //bakery's database of orders
 
-	string name; //customer name
-	double money; //double for the receipt total
-	int count; //represents number of items purchased in the order - like a costco item count
-	string *items; //dynamic array of an itemized list of an order
+    string name; //customer name
+    double money; //double for the receipt total
+    int count; //represents number of items purchased in the order - like a costco item count
+    string *items; //dynamic array of an itemized list of an order
         
 };
 
+
+//createOrder() takes user input to create an Order object
+//arguments: none
+//returns: an Order struct based upon the user's input
 Order createOrder(){
-	Order temp;
-        temp.items = nullptr;
+    
+    Order temp;
+    temp.items = nullptr;
 
-	cout << "Customer name: ";
-	getline(cin, temp.name);
+    cout << "Customer name: ";
+    getline(cin, temp.name);
 
-	cout << "Order total: $";
-	cin >> temp.money;
+    cout << "Order total: $";
+    cin >> temp.money;
 
-	cout << "Item count: ";
-	cin >> temp.count;
-        cin.ignore();        
+    cout << "Item count: ";
+    cin >> temp.count;
+    cin.ignore();        
 
-	temp.items = new string[temp.count];
-	for (int i = 0; i < temp.count; i++){
-		cout << "Item " << (i+1) << ": ";
-		getline(cin, temp.items[i]);
+    temp.items = new string[temp.count];
+    for (int i = 0; i < temp.count; i++){
+        cout << "Item " << (i+1) << ": ";
+        getline(cin, temp.items[i]);
+    }
 
-	}
 
-
-	// returns: a Order struct based upon the user's input
-	return temp;
+    // returns: a Order struct based upon the user's input
+    return temp;
 
 }
 
-
+// displayOrder() outputs an Order object's data
+// arguments: takes in an order object to use to output its information
+// returns: nothing
 void displayOrder(Order o){
 
-	cout << "\nCustomer name: " << o.name;
-	cout << "\nOrder total: $" << o.money;
-	cout << "\nItems: ";
-	for (int i = 0; i < o.count; i++){
-		if (i == 0)
-			cout << o.items[i];
-		else
-			cout << ", " << o.items[i];
-	} 
-        cout<< '\n' << endl;
+    cout << "\nCustomer name: " << o.name;
+    cout << "\nOrder total: $" << o.money;
+    cout << "\nItems: ";
+    for (int i = 0; i < o.count; i++){
+	if (i == 0)
+	cout << o.items[i];
+	else
+        cout << ", " << o.items[i];
+    } 
+    cout<< '\n' << endl;
+}
+
+void displayArray(Order *arr){
+    
+
 }
 
 int main(){
 
-	/*
-	   Customer *arr = nullptr;
-	 *arr //make the size variable (maybe edit the orders array to have a variable size too)
-	 */
+	
+    Customer *arr = nullptr;
+    *arr //make the size variable (maybe edit the orders array to have a variable size too)
+	 
 
-	Order test = createOrder();
-	displayOrder(test);
+    Order test = createOrder();
+    displayOrder(test);
 
+    delete [] arr;
 
 }
 
